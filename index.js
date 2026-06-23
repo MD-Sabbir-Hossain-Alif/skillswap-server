3bfa99b3bce5f6b86366db96eed46f84df4ca83a
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2026-06-24 00:46:15 +0600

    tasks api added

---
+++ index.js
@@ -0,0 +1,63 @@
+// dns server for mongodb connection
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]); // Cloudflare + Google DNS
+
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+var cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.SERVER_PORT
+const uri = process.env.MONGODB_URI
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const db = client.db("skillswap");
+        const tasksCollection = db.collection("tasks");
+
+        app.post('/api/tasks', async (req, res) => {
+            const task = req.body;
+            console.log(task);
+            const newTask = {
+                ...task,
+                createdAt: new Date()
+            }
+            const result = await tasksCollection.insertOne(newTask);
+            res.send(result)
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
